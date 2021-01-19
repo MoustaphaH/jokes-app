106888caf96946399a1420becbfacb6af384b85e
MoustaphaH <mmuhammedzaki@gmail.com>
2021-01-19 12:53:01 +0200

    Java Configuration

--- src/main/java/mdev/springframework/jokesapp/Services/JokeServiceImpl.java
+++ src/main/java/mdev/springframework/jokesapp/Services/JokeServiceImpl.java
@@ -7,9 +7,10 @@ import org.springframework.stereotype.Service;
 public class JokeServiceImpl implements JokeService{
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public JokeServiceImpl(){
-    this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
+
     @Override
     public String getJoke() {
         return chuckNorrisQuotes.getRandomQuote();
